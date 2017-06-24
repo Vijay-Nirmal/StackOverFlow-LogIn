8963f757dd2f6309fa65fe4268c33ffe690e9822
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2017-06-24 11:09:34 +0530

    Initial commit

---
+++ StackOverFlowLogIn/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StackOverFlowLogIn
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //Designed and Developed by Vijay Nirmal(@_Vijay_Nirmal_)
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void UserNameButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var functionString = string.Format(@"document.getElementById('email').innerText = '{0}';", EmailTextBox.Text);
+            await webView.InvokeScriptAsync("eval", new string[] { functionString });
+        }
+
+        private async void PasswordButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var functionString = string.Format(@"document.getElementById('password').innerText = '{0}';", PasswordInputBox.Password);
+            await webView.InvokeScriptAsync("eval", new string[] { functionString });
+        }
+
+        private async void ClickIt_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var functionString = string.Format(@"document.getElementById('submit-button').click();");
+            await webView.InvokeScriptAsync("eval", new string[] { functionString });
+        }
+
+    }
+}
